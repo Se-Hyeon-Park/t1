c484d1e9e7b4a89339cc0743fe55124477491117
SehyeonPark <sehyeon.park@ptbwa.com>
2022-01-13 16:12:08 +0900

    #3 [GitRepoBackup] txt 파일에서 읽어오기 실패 예외 처리 추가

--- GitRepoBackup/git-backup-File.bat
+++ GitRepoBackup/git-backup-File.bat
@@ -1,3 +1,5 @@
+@ECHO OFF
+
 @REM parameter
 SET backupRepoName=Backup Repository Url=
 SET targetRepoName=Target Repository Url=
@@ -7,7 +9,23 @@ FOR /f %%i IN ('powershell -c "get-date -format yyyyMMddHHmmss"') DO SET timesta
 SET dirPath=backup_%timestamp%
 
 @REM get repo address from txt file
+IF NOT EXIST info.txt (
+    SET errFileExist=Please check 'info.txt' exist
+    goto _fileError
+)
 FOR /f "tokens=1,2* delims={}" %%i IN (info.txt) DO IF "%%i"=="%backupRepoName%" ( SET backupRepo=%%j ) ELSE ( IF "%%i"=="%targetRepoName%" ( SET targetRepo=%%j ))
+
+@REM error check
+:_fileError
+IF "%backupRepo%"=="" (
+    SET flg=T
+    SET errBackupUrl=Please check Backup Repository Url
+)
+IF "%targetRepo%"=="" (
+    SET flg=T
+    SET errTargetUrl=Please check Target Repository Url
+)
+IF "%flg%"=="T" goto :_end 
 
 @REM clone the origin repository
 git clone %backupRepo% %dirPath%
@@ -21,4 +39,8 @@ git checkout %dirPath%
 git remote set-url --push origin %targetRepo%
 git push origin %dirPath%
 
+:_end
+ECHO %errFileExist%
+ECHO %errBackupUrl%
+ECHO %errTargetUrl%
 pause
